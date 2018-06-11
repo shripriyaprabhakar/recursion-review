46f9c664edfd02acf6dec0c534e6df472479d358
shripriyaprabhakar <shripriyaprabhakar@gmail.com>
2018-06-11 12:06:04 -0700

    pass Pomander tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,6 @@
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
 
-// but you don't so you're going to write it from scratch:
-
 var stringifyJSON = function(obj) {
   if (typeof obj === 'number' || typeof obj === 'boolean' ||
     obj === null) {
