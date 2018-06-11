ac5d0224caa0c9120f1d46cabb9fad295bc6bee2
shripriyaprabhakar <shripriyaprabhakar@gmail.com>
2018-06-11 12:36:57 -0700

    working on getElementByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,34 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className , node) {
   // your code here
+/*
+i/p : classname string
+o/p: array of nodes
+constraints: none
+edge cases: case where body element has no children
+
+//pseudocode:
+ //var getElementsByClassName = function(className, node) {
+  // declare a results = [];
+   //if(node.classList.indexOf(className) >=  0) {
+         add node to results
+     }
+  //for var i=0; i<node.children.length, increment i
+      // declare childnodesArr = c
+ // results.concat(childnodesArr)
+  // return results
+}
+*/
+  node = node || document.body;
+  var results = [];
+  if (node.classList.indexOf(className) >= 0) {
+    results.push(node);
+  } 
+  for (var i = 0; i < node.children.length; i++) {
+    var childnodesArr = getElementsByClassName(className, node.children[i]);
+    results.concat(childnodesArr);
+  }
+  return results;
 };
