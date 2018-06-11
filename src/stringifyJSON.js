8f71663568afadea4463fda3c056ff1dd483f10a
shripriyaprabhakar <shripriyaprabhakar@gmail.com>
2018-06-11 12:01:46 -0700

    delete comments

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,43 +4,12 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // ioce
-  /* input: number, string, boolean, null, undefined, object, array
-  *  output: stringified data
-  *  constraints: none
-  *  edge case: undefined value in a key-value pair, e.g., {"foo": undefined} -> "{}"
-   pseudocode:
-
-   var stringifyJSON = function(obj) {
-    //if (typeof obj === 'number' || typeof obj === 'boolean' || typeof obj === 'null')
-      //'' + number;
-    //if (typeof obj === 'string')
-      '\"' + string +'\"';
-    //if (typeof obj === 'array') {
-      for each element in array, call stringifyJSON on element
-      '['+resarr+']'
-      resarr = ['str1', 'str2', etc.]
-    }
-    else
-      declare resarr
-      for each key in the object, convert key to string 
-        var key = '\"' + key + '\":'
-        if (obj[key] === undefined) continue
-        var value = stringifyJSON(obj[key])
-        push (key + value) to an array
-      '{'+resarr+'}'      
-  }
-  */
-
   if (typeof obj === 'number' || typeof obj === 'boolean' ||
     obj === null) {
     return '' + obj;
   }
   if (typeof obj === 'string') {
     return '\"' + obj + '\"';
-  }
-  if (typeof obj === 'undefined') {
-    return obj;
   }
   var results = [];
   if (Array.isArray(obj)) {
@@ -50,13 +19,16 @@ var stringifyJSON = function(obj) {
     });
     return '[' + results + ']';
   }
-  for (var key in obj) {
-    var keyString = '\"' + key + '\":';
-    if (obj[key] === undefined || typeof obj[key] === 'function') {
-      continue;
+  if (obj) {
+    for (var key in obj) {
+      var keyString = '\"' + key + '\":';
+      if (obj[key] === undefined || typeof obj[key] === 'function') {
+        continue;
+      }
+      var valString = stringifyJSON(obj[key]);
+      results.push(keyString + valString);
     }
-    var valString = stringifyJSON(obj[key]);
-    results.push(keyString + valString);
+    return '{' + results + '}';
   }
-  return '{' + results + '}';
+  return 'undefined';
 };
